f25304694744115fd1546f1e7a5932ec2f6d391a
Saleh Ahmad Shakil <>
2022-08-25 21:30:30 +0600

    27th class with filter and order models

--- apps/frontend/views.py
+++ apps/frontend/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, DetailView, ListView
 
 from apps.frontend.models import HomeSlider
-from apps.product.models import Category, Product, ProductPhoto
+from apps.product.models import Category, Product, ProductPhoto, Color
 
 
 class HomeView(TemplateView):
@@ -29,11 +29,23 @@ class ShopView(ListView):
         qs = super(ShopView, self).get_queryset()
         lr = self.request.GET.get('lower', None)
         up = self.request.GET.get('upper', None)
+        cat = self.request.GET.get('cat', None)
+        color = self.request.GET.get('color', None)
+        if color:
+            qs = qs.filter(color=color)
+        if cat:
+            qs = qs.filter(category=cat)
         if lr:
             qs = qs.filter(price__gte=lr)
         if up:
             qs = qs.filter(price__lte=up)
         return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cat_list'] = Category.objects.all()
+        context['color_list'] = Color.objects.all()
+        return context
 
 
 class ProductView(DetailView):
--- apps/order/models.py
+++ apps/order/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import SET_NULL
+
+from apps.account.models import Customer
+from apps.product.models import Product
+
+
+class Order(models.Model):
+    class OrderChoices(models.IntegerChoices):
+        PENDING = 1, 'Pending'
+        IN_PROCESS = 2, 'In Process'
+        DELIVERED = 3, 'Delivered'
+        CANCEL = 4, 'Cancel'
+
+    class PaymentChoices(models.IntegerChoices):
+        PAID = 1, 'Paid'
+        UN_PAID = 2, 'Un Paid'
+
+    code = models.CharField(max_length=20, null=True, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=SET_NULL, null=True, blank=True, related_name='customer_order')
+    amount = models.FloatField(default=0)
+    coupon = models.CharField(max_length=50)
+    discount = models.FloatField(default=0)
+    total = models.FloatField(default=0)
+    date = models.DateField(auto_now_add=True)
+    status = models.SmallIntegerField(choices=OrderChoices.choices, default=1)
+    payment = models.SmallIntegerField(choices=PaymentChoices.choices, default=1)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_item')
+    product = models.ForeignKey(Product, on_delete=SET_NULL, null=True, blank=True)
+    unit = models.FloatField(default=0)
+    qty = models.FloatField(default=0)
+    price = models.FloatField(default=0)
